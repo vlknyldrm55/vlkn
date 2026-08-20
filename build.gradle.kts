23f58a1e7779f7e8c00c31a0e4d78abb253a7321
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-20 19:57:50 +0300

    Update repository URL in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/feroxx/Kekik-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/vlknyldrm55/vlkn")
 
         authors = listOf("keyiflerolsun")
     }
