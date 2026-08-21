baa29337f715739ecc499c880e9ff2918cf7b183
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-21 08:29:08 +0300

    Update vyPlugin.kt

--- vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
+++ vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
@@ -4,10 +4,9 @@ import android.app.AlertDialog
 import android.content.Context
 import android.text.InputType
 import android.widget.EditText
+import androidx.preference.PreferenceManager
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.utils.DataStoreHelper.setKey
-import com.lagradost.cloudstream3.utils.DataStoreHelper.getKey
 
 @CloudstreamPlugin
 class vyPlugin: Plugin() {
@@ -16,19 +15,20 @@ class vyPlugin: Plugin() {
     }
 
     override fun openSettings(context: Context) {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Erişim Şifresi")
 
         val input = EditText(context)
         input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
         input.hint = "Şifrenizi girin"
-        input.setText(getKey<String>("vy_password") ?: "")
+        input.setText(sharedPref.getString("vy_password", "") ?: "")
 
         builder.setView(input)
 
         builder.setPositiveButton("Kaydet") { dialog, _ ->
             val enteredPassword = input.text.toString()
-            setKey("vy_password", enteredPassword)
+            sharedPref.edit().putString("vy_password", enteredPassword).apply()
             dialog.dismiss()
         }
         builder.setNegativeButton("İptal") { dialog, _ -> dialog.cancel() }
