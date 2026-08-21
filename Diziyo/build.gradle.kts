3f1444a56577cd82f79e0acee27d9a37d38da3ed
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-21 06:03:06 +0300

    Add files via upload

---
+++ Diziyo/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("hexated", "keyiflerolsun")
+    language    = "tr"
+    description = "Diziyo Cloudstream Eklentisi"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     **/
+    status      = 1 // will be 3 if unspecified
+    tvTypes     = listOf("TvSeries")
+    iconUrl     = "https://www.google.com/s2/favicons?domain=https://www.diziyo.so/&sz=%size%"
+}
