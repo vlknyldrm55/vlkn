4426932295f6c737859d72dd89b596fcc7143ee8
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-21 07:59:30 +0300

    Implement settings dialog for password management
    Add a settings dialog for password input and storage

--- vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
+++ vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
@@ -1,12 +1,36 @@
 package com.keyiflerolsun
 
+import android.content.Context
+import androidx.preference.PreferenceManager
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class vyPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(vy())
+    }
+
+    override fun openSettings(context: Context) {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        
+        val builder = android.app.AlertDialog.Builder(context)
+        builder.setTitle("Erişim Şifresi")
+        
+        val input = android.widget.EditText(context)
+        input.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+        input.hint = "Şifrenizi girin"
+        input.setText(sharedPref.getString("vy_password", ""))
+        
+        builder.setView(input)
+
+        builder.setPositiveButton("Kaydet") { dialog, _ ->
+            val enteredPassword = input.text.toString()
+            sharedPref.edit().putString("vy_password", enteredPassword).apply()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("İptal") { dialog, _ -> dialog.cancel() }
+
+        builder.show()
     }
 }
