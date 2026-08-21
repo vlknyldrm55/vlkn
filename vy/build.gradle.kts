e025fddf504782875112028f80e0ddc6c37ba078
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-21 07:44:04 +0300

    Add files via upload

---
+++ vy/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("hexated", "keyiflerolsun")
+    language    = "tr"
+    description = "vy Canlı TV Eklentisi"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     **/
+    status      = 1
+    tvTypes     = listOf("Live")
+    iconUrl     = "https://i.imgur.com/J7dBspr.png"
+}
