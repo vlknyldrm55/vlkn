8ba839d211285c4653c1105a2265c75e66e21e3f
Volkan <81734815+vlknyldrm55@users.noreply.github.com>
2026-08-21 08:41:04 +0300

    Update vyPlugin.kt

--- vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
+++ vy/src/main/kotlin/com/keyiflerolsun/vyPlugin.kt
@@ -1,9 +1,6 @@
 package com.keyiflerolsun
 
-import android.app.AlertDialog
 import android.content.Context
-import android.text.InputType
-import android.widget.EditText
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
@@ -11,27 +8,5 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class vyPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(vy())
-    }
-
-    override fun openSettings(context: Context) {
-        val pluginApi = vy()
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle("Erişim Şifresi")
-
-        val input = EditText(context)
-        input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-        input.hint = "Şifrenizi girin"
-        input.setText(pluginApi.getKey<String>("vy_password") ?: "")
-
-        builder.setView(input)
-
-        builder.setPositiveButton("Kaydet") { dialog, _ ->
-            val enteredPassword = input.text.toString()
-            pluginApi.setKey("vy_password", enteredPassword)
-            dialog.dismiss()
-        }
-        builder.setNegativeButton("İptal") { dialog, _ -> dialog.cancel() }
-
-        builder.show()
     }
 }
